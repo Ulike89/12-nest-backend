97fa014082a5f828a43219e6ebcfed74c7366499
Julio Cesar Monroy Casimiro <julio.monroy@tucasaexpress.com>
2025-02-17 18:43:35 -0600

    Nest Backend

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+
+@Module({
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI', 'mongodb://localhost:27017/authentication-db')
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {
+}
